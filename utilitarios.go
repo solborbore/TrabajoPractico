c7e7bbe7c4faabd443f1bed52996cfcd1ec84cd2
Juan Stepanenko <Juan Stepanenko>
2016-11-16 16:33:46 -0300

    Primer commit

---
+++ utilitarios.go
@@ -0,0 +1,27 @@
+package main
+
+func sumaDeNumeros(numeros []int) (sum int) {
+	for _, num := range numeros {
+		sum += num
+	}
+	return
+}
+
+func largoDeCadenas(cadenas []string) int {
+	sumaCaracteres := 0
+	for _, cadena := range cadenas {
+		sumaCaracteres += len(cadena)
+	}
+	return sumaCaracteres
+}
+
+type a interface{}
+
+func any(vs []iContenido, f func(iContenido) bool) bool {
+	for _, v := range vs {
+		if f(v) {
+			return true
+		}
+	}
+	return false
+}
