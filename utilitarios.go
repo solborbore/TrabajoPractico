2147bbc69dc3670c878966ec09ffafa375274391
FischerMatias <mfischer00@gmail.com>
2016-11-17 23:05:44 -0300

    agregado all

--- utilitarios.go
+++ utilitarios.go
@@ -25,3 +25,12 @@ func any(vs []iContenido, f func(iContenido) bool) bool {
 	}
 	return false
 }
+
+func all(vs []iContenido, f func(iContenido) bool) bool {
+	for _, v := range vs {
+		if !f(v) {
+			return false
+		}
+	}
+	return true
+}
